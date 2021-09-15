e3dd1d12a576d01ea4f259762bb850b953d874c1
Nataliia Pogrebna <natalibunny13@gmail.com>
2021-09-15 12:40:37 +0300

    add libraries, conditions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './sass/main.scss';
 import axios from "axios";
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// all modules
+import Notiflix from "notiflix";
 import objectJS from './object'
 
 
@@ -11,16 +15,44 @@ const refs = {
     loadmoreBtn: document.querySelector(".load-more"),
 };
 
+let last = 0;
 
 const getImages = async () => {
   try {
 
     const { hits: images, totalHits } = await objectJS.fetchImages();
     renderGallery(images);
-    
-    // console.log(images);
-  } catch (error) {
-    console.error("Sorry, there are no images matching your search query. Please try again.", error);
+    if (images.length === 0) {
+      Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    }
+    new SimpleLightbox('.gallery a', { 
+      showCounter: false,
+      disableScroll: false,
+      captionsData: 'alt',
+      captionDelay: 250,
+      docClose: true,
+  });
+
+  const { height: cardHeight } = document
+  .querySelector('.gallery')
+  .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+  top: cardHeight * 2,
+  behavior: 'smooth',
+  });
+
+    last += 40;
+    console.log(last);
+    console.log(totalHits);
+    // console.log(images.length);
+    if (last >= totalHits && last !== 0) {
+      Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+      refs.loadmoreBtn.setAttribute("hidden", true);
+    }
+    console.log(images);
+    } catch (error) {
+    console.log(error);
   }
 }
 
@@ -29,8 +61,8 @@ const handleSubmit = e => {
   e.preventDefault();
   objectJS.query = refs.input.value;
   clearGallery();
-  
   getImages();
+  refs.loadmoreBtn.removeAttribute("hidden", true);
 }
 
 refs.form.addEventListener("submit", handleSubmit);
@@ -39,7 +71,9 @@ function renderGallery(images) {
   console.log(images);
     const markup = images.map((image => {
         return `<div class="photo-card">
+        <a class="gallery__link" href="${image.largeImageURL}">
         <img src="${image.webformatURL}" alt="${image.tags}" width="350" loading="lazy" />
+        </a>
         <div class="info">
           <p class="info-item">
             <b>Likes: </b>${image.likes}
@@ -58,36 +92,30 @@ function renderGallery(images) {
         })).join('');
         refs.gallery.insertAdjacentHTML('beforeend', markup);
         return refs.gallery;
-  }
+}
   
+  
+
   // refs.body.insertAdjacentElement('afterbegin', refs.gallery);
-  
-  
+refs.loadmoreBtn.setAttribute("hidden", true);
 
-  refs.loadmoreBtn.addEventListener("click", onLoadMore);
+refs.loadmoreBtn.addEventListener("click", onLoadMore);
 
-  function onLoadMore() {
+function onLoadMore() {
     console.log(objectJS.page);
     objectJS.page += 1;
     getImages();
     console.log(objectJS.page);
-  }
+   
+}
 
-  const clearGallery = () => {
+const clearGallery = () => {
     if (refs.gallery.children.length) {
       refs.gallery.innerHTML = '';
     }
-  };
+    objectJS.page = 1;
+};
 
-
-
-  // let inputField = '';
-
-// const inputHandler = (e)=> {
-//   inputField= e.target.value;
-// }
-
-// refs.input.addEventListener('input', inputHandler);
 
 
 
